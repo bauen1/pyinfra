2d21b005a0465fd525e1826432a89cec2754c708
bauen1 <j2468h@gmail.com>
2024-10-05 17:26:12 +0200

    pyinfra/api: explicitly reexport

--- pyinfra/api/__init__.py
+++ pyinfra/api/__init__.py
@@ -24,3 +24,32 @@ from .host import Host  # noqa: F401 # pragma: no cover
 from .inventory import Inventory  # noqa: F401 # pragma: no cover
 from .operation import operation  # noqa: F401 # pragma: no cover
 from .state import BaseStateCallback, State  # noqa: F401 # pragma: no cover
+
+# Explicitly re-export all imports, as required per
+# <https://github.com/microsoft/pyright/blob/main/docs/typed-libraries.md#library-interface>
+__all__ = [
+    "FileDownloadCommand",
+    "FileUploadCommand",
+    "FunctionCommand",
+    "MaskString",
+    "QuoteString",
+    "RsyncCommand",
+    "StringCommand",
+    "Config",
+    "deploy",
+    "DeployError",
+    "FactError",
+    "FactTypeError",
+    "FactValueError",
+    "InventoryError",
+    "OperationError",
+    "OperationTypeError",
+    "OperationValueError",
+    "FactBase",
+    "ShortFactBase",
+    "Host",
+    "Inventory",
+    "operation",
+    "BaseStateCallback",
+    "State",
+]
